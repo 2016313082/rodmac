1ae8a4932a82e115ffb6fe59e62e5b3f80d727a3
josepueblito <85978161+JosePueblito@users.noreply.github.com>
2022-10-08 21:39:47 -0500

    Ultimas modificaciones en rodmac, Validacion para nivel cliente, Vista de tienda, Plantilla cargada en tienda_view junto con estilos y scripts propios en carpetas cliente

--- application/controllers/Cliente.php
+++ application/controllers/Cliente.php
@@ -12,5 +12,9 @@
 			$this->load->view('cliente/cliente_view', $this->data);
 		}
 
+		public function tienda(){
+			$this->data['datos_pagina']['titulo_pagina'] = "Cliente";
+			$this->load->view('tienda/tienda_view', $this->data);
+		}
 	}
 ?>
